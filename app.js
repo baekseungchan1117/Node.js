9addd276cda98c062d955c0064453bc4fba9ae89
baekseungchan1117 <bsc7386@gmail.com>
2023-05-22 22:41:16 +0900

    5월 22일 연습

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path')
+
+const app = express();
+
+const adminRouter = require('./routes/admin')
+const shopRouter = require('./routes/shop')
+
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use('/admin',adminRouter)
+app.use(shopRouter)
+
+app.use('/', (req, res, next) =>{
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
+
+app.listen(8000);
---
+++ routes/admin.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path')
+
+
+const router = express.Router();
+
+
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+});
+
+
+
+// /admin/product => POST
+router.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+
+module.exports = router;
